f77847a2d451f9bb18222ad94e5f2db68f853695
Sam Holmes <samholmes1337@gmail.com>
2016-05-31 16:39:36 +0100

    Render views from views directory

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var app = express();
 
 // Configuration
 var port = process.env.PORT || 8080;
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/public/views");
+
+// Middleware
+app.use(express.static(__dirname + "/public"));
 
 // Primary routes
 app.get("/", function(req, res){
-    res.send("Welcome!");
+    res.render("index");
 });
 
 // Listen
