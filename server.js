9a79eb27acaf5418067dc858a8d8d37123448d8a
Sam Holmes <samholmes1337@gmail.com>
2016-11-15 11:31:44 +0000

    Update package dependencies

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-// Dependencies
-var express = require("express");
-var app = express();
-var path = require("path");
-var bytes = require("bytes");
-var numeral = require("numeral");
-var low = require("lowdb");
-var Stream = require("stream");
-var progress = require("progress-stream");
+"use strict";
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const bytes = require("bytes");
+const numeral = require("numeral");
+const low = require("lowdb");
+const Stream = require("stream");
+const progress = require("progress-stream");
 
 // Database setup
-var db = low(__dirname + "/db/low.json");
+let db = low(__dirname + "/db/low.json");
 db.defaults({
     stats: {
         downloads: 0,
@@ -22,7 +23,7 @@ db.defaults({
 }).value();
 
 // Configuration
-var port = process.env.PORT || 8080;
+let port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -33,11 +34,11 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
 
     // Get database values
-    var downloads = numeral(db.get("stats.downloads").value()).format("0,0");
-    var speedTotal = db.get("stats.speed.total").value();
-    var speedCount = db.get("stats.speed.count").value();
-    var speedAverage = bytes(speedTotal / speedCount) + "/s";
-    var transfer = bytes(db.get("stats.transfer").value());
+    let downloads = numeral(db.get("stats.downloads").value()).format("0,0");
+    let speedTotal = db.get("stats.speed.total").value();
+    let speedCount = db.get("stats.speed.count").value();
+    let speedAverage = bytes(speedTotal / speedCount) + "/s";
+    let transfer = bytes(db.get("stats.transfer").value());
 
     res.render("index", {
         stats: {
@@ -52,11 +53,11 @@ app.get("/", (req, res) => {
 app.get("/:filesize/:filename", (req, res) => {
 
     // Get request parameters
-    var fileSize = req.params.filesize;
-    var fileName = req.params.filename;
+    let fileSize = req.params.filesize;
+    let fileName = req.params.filename;
 
     // Attempt to parse the size
-    var contentSize = bytes.parse(fileSize);
+    let contentSize = bytes.parse(fileSize);
 
     // Make sure valid filesize
     if (isNaN(contentSize) || contentSize === null) {
@@ -110,10 +111,10 @@ app.get("/:filesize/:filename", (req, res) => {
         }
 
         // Create new stream
-        var rs = new DataStream();
+        let rs = new DataStream();
 
         // Create stream monitor
-        var monitor = progress({
+        let monitor = progress({
             length: contentSize,
             time: 1000,
             speed: 0
