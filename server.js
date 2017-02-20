71ef6936a8fef0f886c3b80d939fa2dc7aad6865
Sam Holmes <samholmes1337@gmail.com>
2017-02-20 20:06:25 +0000

    Tidy up formatting and fix db bug

--- server.js
+++ server.js
@@ -6,20 +6,29 @@ const path = require("path");
 const bytes = require("bytes");
 const numeral = require("numeral");
 const low = require("lowdb");
+const fs = require("fs");
 const Stream = require("stream");
 const progress = require("progress-stream");
 
+const config = {
+  dir: path.resolve(__dirname, "./db"),
+  name: "low.json"
+};
+
+if (!fs.existsSync(config.dir))
+  fs.mkdirSync(config.dir);
+
 // Database setup
-let db = low(__dirname + "/db/low.json");
+let db = low(path.resolve(config.dir, config.name));
 db.defaults({
-    stats: {
-        downloads: 0,
-        transfer: 0,
-        speed: {
-            total: 500000,
-            count: 1
-        }
+  stats: {
+    downloads: 0,
+    transfer: 0,
+    speed: {
+      total: 500000,
+      count: 1
     }
+  }
 }).value();
 
 // Configuration
@@ -33,120 +42,120 @@ app.use(express.static(__dirname + "/public"));
 // Primary routes
 app.get("/", (req, res) => {
 
-    // Get database values
-    let downloads = numeral(db.get("stats.downloads").value()).format("0,0");
-    let speedTotal = db.get("stats.speed.total").value();
-    let speedCount = db.get("stats.speed.count").value();
-    let speedAverage = bytes(speedTotal / speedCount) + "/s";
-    let transfer = bytes(db.get("stats.transfer").value());
+  // Get database values
+  let downloads = numeral(db.get("stats.downloads").value()).format("0,0");
+  let speedTotal = db.get("stats.speed.total").value();
+  let speedCount = db.get("stats.speed.count").value();
+  let speedAverage = bytes(speedTotal / speedCount) + "/s";
+  let transfer = bytes(db.get("stats.transfer").value());
 
-    res.render("index", {
-        stats: {
-            downloads: downloads,
-            speed: speedAverage,
-            transfer: transfer
-        }
-    });
+  res.render("index", {
+    stats: {
+      downloads: downloads,
+      speed: speedAverage,
+      transfer: transfer
+    }
+  });
 });
 
 // Primary API route
 app.get("/:filesize/:filename", (req, res) => {
 
-    // Get request parameters
-    let fileSize = req.params.filesize;
-    let fileName = req.params.filename;
+  // Get request parameters
+  let fileSize = req.params.filesize;
+  let fileName = req.params.filename;
 
-    // Attempt to parse the size
-    let contentSize = bytes.parse(fileSize);
+  // Attempt to parse the size
+  let contentSize = bytes.parse(fileSize);
 
-    // Make sure valid filesize
-    if (isNaN(contentSize) || contentSize === null) {
+  // Make sure valid filesize
+  if (isNaN(contentSize) || contentSize === null) {
 
-        res.json({
-            success: false,
-            message: "Could not parse the filesize '" + fileSize + "' into bytes"
-        });
+    res.json({
+      success: false,
+      message: "Could not parse the filesize '" + fileSize + "' into bytes"
+    });
 
-    } else {
+  } else {
 
-        // Increment the download counter
-        db.set("stats.downloads", db.get("stats.downloads").value() + 1).value();
+    // Increment the download counter
+    db.set("stats.downloads", db.get("stats.downloads").value() + 1).value();
 
-        // Set the response headers
-        res.writeHead(200, {
-            "Content-Type": "application/octet-stream",
-            "Content-Disposition": "attachment; filename=" + fileName,
-            "Content-Length": contentSize
-        });
+    // Set the response headers
+    res.writeHead(200, {
+      "Content-Type": "application/octet-stream",
+      "Content-Disposition": "attachment; filename=" + fileName,
+      "Content-Length": contentSize
+    });
 
-        // Define readable stream class
-        class DataStream extends Stream.Readable {
+    // Define readable stream class
+    class DataStream extends Stream.Readable {
 
-            // Construct the class
-            constructor(opt) {
-                super(opt);
-                this._remaining = contentSize;
-            }
+      // Construct the class
+      constructor(opt) {
+        super(opt);
+        this._remaining = contentSize;
+      }
 
-            // When the class is read
-            _read(size) {
-                if (this._remaining === 0) {
-                    this.push(null);
-                } else {
-                    if (this._remaining < size) {
+      // When the class is read
+      _read(size) {
+        if (this._remaining === 0) {
+          this.push(null);
+        } else {
+          if (this._remaining < size) {
 
-                        // Push the remaining, set to 0 and finish read
-                        this.push(Buffer.alloc(this._remaining));
-                        this._remaining = 0;
+            // Push the remaining, set to 0 and finish read
+            this.push(Buffer.alloc(this._remaining));
+            this._remaining = 0;
 
-                    } else {
+          } else {
 
-                        // Push the size being requested
-                        this.push(Buffer.alloc(size));
-                        this._remaining = this._remaining - size;
+            // Push the size being requested
+            this.push(Buffer.alloc(size));
+            this._remaining = this._remaining - size;
 
-                    }
-                }
-            }
+          }
         }
-
-        // Create new stream
-        let rs = new DataStream();
-
-        // Create stream monitor
-        let monitor = progress({
-            length: contentSize,
-            time: 1000,
-            speed: 0
-        });
-
-        // When progress is detected
-        monitor.on('progress', progress => {
-
-            // Update statistics
-            db.set("stats.transfer", db.get("stats.transfer").value() + progress.delta).value();
-            db.set("stats.speed.count", db.get("stats.speed.count").value() + 1).value();
-            db.set("stats.speed.total", db.get("stats.speed.total").value() + progress.speed).value();
-
-        });
-
-        // Pipe output to response
-        rs.pipe(monitor).pipe(res);
-
-        // Additional debugging commands
-        // res.on("close", () => { console.log("Response closed"); });
-        // res.on("end", () => { console.log("Response ended"); });
-        // req.on("close", () => { console.log("Request closed"); });
-        // req.on("end", () => { console.log("Request ended"); });
-        // rs.on("end", () => { console.log("Stream ended"); });
-        // rs.on("close", () => { console.log("Stream closed"); });
-
+      }
     }
+
+    // Create new stream
+    let rs = new DataStream();
+
+    // Create stream monitor
+    let monitor = progress({
+      length: contentSize,
+      time: 1000,
+      speed: 0
+    });
+
+    // When progress is detected
+    monitor.on('progress', progress => {
+
+      // Update statistics
+      db.set("stats.transfer", db.get("stats.transfer").value() + progress.delta).value();
+      db.set("stats.speed.count", db.get("stats.speed.count").value() + 1).value();
+      db.set("stats.speed.total", db.get("stats.speed.total").value() + progress.speed).value();
+
+    });
+
+    // Pipe output to response
+    rs.pipe(monitor).pipe(res);
+
+    // Additional debugging commands
+    // res.on("close", () => { console.log("Response closed"); });
+    // res.on("end", () => { console.log("Response ended"); });
+    // req.on("close", () => { console.log("Request closed"); });
+    // req.on("end", () => { console.log("Request ended"); });
+    // rs.on("end", () => { console.log("Stream ended"); });
+    // rs.on("close", () => { console.log("Stream closed"); });
+
+  }
 
 });
 
 app.get("*", (req, res) => {
-    res.redirect("/");
+  res.redirect("/");
 });
 
 // Listen
